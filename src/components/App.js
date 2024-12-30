ab9108ba38315a617ea116521b09e9c59aae44a1
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-12-30 11:29:27 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
+import React, { useState } from "react";
 
+const App = () => {
+  const [numbers, setNumbers] = useState([]);
+  const [sum, setSum] = useState(0);
+
+  const handleInputChange = (e) => {
+    const newNumber = e.target.value;
+    if (newNumber) {
+      const updatedNumbers = [...numbers, parseInt(newNumber)];
+      setNumbers(updatedNumbers);
+      setSum(updatedNumbers.reduce((acc, num) => acc + num, 0));
+    }
+  };
+
+  return (
+    <div>
+      <h1>Sum Calculator</h1>
+      <input
+        type="number"
+        onChange={handleInputChange}
+        placeholder="Enter a number"
+      />
+      <div>
+        <h3>Sum: {sum}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+/*
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
+        {/* Do not remove the main div */
+   /* </div>
   )
 }
 
-export default App
+export default App*/
