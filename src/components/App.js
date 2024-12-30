3e604e554924458d3f966c80985c695ddca1a604
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-12-30 11:35:02 +0530

    code modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,39 @@
 import React, { useState } from "react";
 
 const App = () => {
+  const [numbers, setNumbers] = useState([]); // To store input numbers
+  const [sum, setSum] = useState(0); // To store the sum of numbers
+
+  const handleInputChange = (e) => {
+    const newNumber = e.target.value;
+    if (newNumber) {
+      const updatedNumbers = [...numbers, parseInt(newNumber)];
+      setNumbers(updatedNumbers);
+      setSum(updatedNumbers.reduce((acc, num) => acc + num, 0)); // Update sum
+    }
+  };
+
+  return (
+    <div>
+      <h1>Sum Calculator</h1>
+      <input
+        type="number"
+        onChange={handleInputChange} // On input change, update numbers and sum
+        placeholder="Enter a number"
+      />
+      <div>
+        <p>Current Sum: {sum}</p> {/* The <p> tag that Cypress tests will look for */}
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+/*
+import React, { useState } from "react";
+
+const App = () => {
   const [numbers, setNumbers] = useState([]);
   const [sum, setSum] = useState(0);
 
