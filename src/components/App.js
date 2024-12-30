da1a425c4b87bd50cf5d8619c5fa3ae4c9d2eb99
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-12-30 11:46:37 +0530

    code1

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ const App = () => {
         placeholder="Enter a number"
       />
       <div>
-        <p>Sum: {sum}</p> {/* The <p> tag that Cypress tests will look for */}
+        <p>Sum: {sum}</p> {/* Changed text to match the test */}
       </div>
     </div>
   );
@@ -30,49 +30,3 @@ const App = () => {
 
 export default App;
 
-/*
-import React, { useState } from "react";
-
-const App = () => {
-  const [numbers, setNumbers] = useState([]);
-  const [sum, setSum] = useState(0);
-
-  const handleInputChange = (e) => {
-    const newNumber = e.target.value;
-    if (newNumber) {
-      const updatedNumbers = [...numbers, parseInt(newNumber)];
-      setNumbers(updatedNumbers);
-      setSum(updatedNumbers.reduce((acc, num) => acc + num, 0));
-    }
-  };
-
-  return (
-    <div>
-      <h1>Sum Calculator</h1>
-      <input
-        type="number"
-        onChange={handleInputChange}
-        placeholder="Enter a number"
-      />
-      <div>
-        <h3>Sum: {sum}</h3>
-      </div>
-    </div>
-  );
-};
-
-export default App;
-
-/*
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */
-   /* </div>
-  )
-}
-
-export default App*/
